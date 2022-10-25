df24e768ba3946955583296091684a4495746780
andres3455 <111390187+andres3455@users.noreply.github.com>
2022-10-25 08:59:26 -0500

    Update segunda_tarea.py

--- segunda_tarea.py
+++ segunda_tarea.py
@@ -180,8 +180,8 @@ def inversa_vector(d):
     for i in range (numero):
         si[i] = op.producto((-1,0),d[i])
     return (si)
+            
 def producto(m1, m2):
-
     if len(m1[0]) == len(m2):
         m = [[(0, 0) for i in range(len(m2[0]))] for j in range(len(m1))]
         for row in range(len(m1)):
